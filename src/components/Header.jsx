2ff9a9b5207069c4bfad43b4bc6130a7722976e7
Oleg Grishko <oleggr-brd@yandex.ru>
2023-03-14 11:54:23 +0300

    begin

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+          <img src='https://avatars.mds.yandex.net/i?id=f13b4e4666cef40f6cb3be6016f6a067f4abee76-9181123-images-thumbs&n=13'/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className='content'>
+          <div>
+            <img src='https://avatars.mds.yandex.net/i?id=dca793c7f8bd10501d5fb7083c762423fcfa939a-8257574-images-thumbs&n=13' />
+          </div>
+          <div>
+            <img src='https://avatars.mds.yandex.net/i?id=b70aab8decbd3b8c992533f5046db92141352487-9065887-images-thumbs&n=13' />
+            ava + description
+          </div>
+          <div>
+            My posts
+            <div>
+              New post
+            </div>
+            <div>
+              <div>
+                Post 1
+              </div>
+              <div>
+                Post 2
+              </div>
+            </div>
+          </div>
+        </div>
+    )
+}
+
+export default Profile;
