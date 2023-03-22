47c10bee242a35d771b4d1df1685ba13498101e8
Oleg Grishko <oleggr-brd@yandex.ru>
2023-03-22 17:06:48 +0300

    dialogs and message

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 
 
@@ -18,6 +21,10 @@ const App = () => {
                     <Routes>
                         <Route path='/dialogs' element={<Dialogs/>}/>
                         <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
+
                     </Routes>
 
                 </div>
